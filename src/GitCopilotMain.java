bed52d4a22bc3cb3dac37ea20eb504d908377e81
srm776 <srmur776@gmail.com>
2023-12-17 13:04:20 -0500

    add co-pilot files

--- src/GitCopilotMain.java
+++ src/GitCopilotMain.java
@@ -1,6 +1,31 @@
-public class GitCopilotMain {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GitCopilotMain {
     public  static  void  main(String[] args){
         System.out.println("GitCopilotMain---");
+        List<Integer> list = new ArrayList();
+        //add numbers to the list
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+        int result = add(1, 2);
+        System.out.println(result);
+        System.out.println(add(list));
+        }
+  //static method that adds two numbers and returns the result
+    public static int add(int num1, int num2) {
+        return num1 + num2;
     }
+    // static method that loops thru a list and adds the numbers and returns the result
+    public static int add(List<Integer> list) {
+        int result = 0;
+        for (int i : list) {
+            result += i;
+        }
+        return result;
+    }
+
 }
