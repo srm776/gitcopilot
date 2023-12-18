861949b54a36bcfa93ef141d30918603583d1ebb
srm776 <srmur776@gmail.com>
2023-12-17 22:39:15 -0500

    standard pojo

---
+++ src/Pojo.java
@@ -0,0 +1,38 @@
+public class Pojo {
+    private String name;
+    private int age;
+    private double salary;
+    private String vehicle;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+}
